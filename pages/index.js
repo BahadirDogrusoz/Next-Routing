3a0332396460cc200c79d424c78428ae1ff327e2
Mustafa Bahadır Doğrusöz <62994896+BahadirDogrusoz@users.noreply.github.com>
2021-10-24 13:34:20 +0300

    First commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function HomePage() {
+    return (
+      <div>
+          <h1>The Home Page</h1>
+      </div>
+    );
+}
+export default HomePage;
---
+++ pages/portfolio/index.js
@@ -0,0 +1,8 @@
+function PortfolioPage() {
+    return (
+      <div>
+          <h1>The Portfolio Page</h1>
+      </div>
+    );
+}
+export default PortfolioPage;
