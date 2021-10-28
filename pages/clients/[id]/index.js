3f413cf4d97d76d444ecad3b1fd8fb36b8c272ba
Mustafa Bahadır Doğrusöz <62994896+BahadirDogrusoz@users.noreply.github.com>
2021-10-28 10:46:38 +0300

    Third Commit

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -5,9 +5,17 @@ function ClientProjectsPage() {
 
     console.log(router.query)
 
+    function loadProjectHandler() {
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: {id: 'bah', clientprojectid: 'projecta'}
+        })
+    }
+
     return(
         <div>
             <h1>The Projects of a Given client</h1>
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     );
 }
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 function ClientsPage() {
+    const clients = [
+        {id: 'bah', name: 'Bahadır'},
+        {id: 'manu', name: 'Manual'},
+    ];
+
     return(
         <div>
             <h1>The Clients Page</h1>
+            <ul>
+                {clients.map(client => <li key={client.id}>
+                    <Link href={{
+                        pathname: '/clients/[id]',
+                        query: {id: client.id},
+                    }}>{client.name}</Link>
+                </li>)}
+            </ul>
         </div>
     );
 }
